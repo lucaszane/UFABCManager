5f5c679940887c653abcf44b9a94b6c996acb5ef
brunoburga <bb_borghese@hotmail.com>
2017-11-06 21:41:14 -0200

    Testando git

--- UFABCManager/src/ufabcmanager/UFABCManager.java
+++ UFABCManager/src/ufabcmanager/UFABCManager.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ufabcmanager;
 
 /**
@@ -11,11 +6,8 @@ package ufabcmanager;
  */
 public class UFABCManager {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("MAIN RUN");
     }
     
 }
